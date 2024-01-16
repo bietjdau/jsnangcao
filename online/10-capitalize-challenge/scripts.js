45d5cd8600dab3a9c868fae027e1015ff15547db
bietjdau <thanhdo112004@gmail.com>
2024-01-16 13:57:41 +0700

    2-3-4 + reup 1

--- online/10-capitalize-challenge/scripts.js
+++
@@ -1,5 +0,0 @@
-const myString = 'developer';
-
-myNewString = myString[0].toUpperCase() + myString.substring(1); // lấy ký tự đầu tạo thành chữ hoa + các ký tự sau của chuỗi để tạo thành chuỗi mới
-
-console.log(myNewString); // 'Developer'
